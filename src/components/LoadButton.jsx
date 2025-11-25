f504ce7233e5cbbff68831b8f8b8ae87635986d2
Luis-Eduardo RUIZ <ruizle@bluewin.ch>
2025-11-25 10:07:28 +0100

    xxx

--- src/components/LoadButton.jsx
+++ src/components/LoadButton.jsx
@@ -5,5 +5,5 @@ export const LoadButton = () => {
   const handleClick = () => {
     load();
   };
-  return <button onClick={handleClick}>load</button>;
+  return <button onClick={handleClick}>load upd</button>;
 };
