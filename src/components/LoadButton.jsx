314ceff455163bdbc1d4089f934ea125f22aca39
Nicolas Fazio <contact@nicolasfazio.ch>
2025-11-27 10:48:02 +0100

    Update button text to 'load upd from GitHub'

--- src/components/LoadButton.jsx
+++ src/components/LoadButton.jsx
@@ -5,5 +5,5 @@ export const LoadButton = () => {
   const handleClick = () => {
     load();
   };
-  return <button onClick={handleClick}>load upd pull request</button>;
+  return <button onClick={handleClick}>load upd from Github</button>;
 };
