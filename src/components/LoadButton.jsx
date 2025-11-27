269b10bb7783ad983fc4985260edbdf0fccfe0fe
Nicolas Fazio <contact@nicolasfazio.ch>
2025-11-27 10:49:43 +0100

    Refactor LoadButton to include a paragraph

--- src/components/LoadButton.jsx
+++ src/components/LoadButton.jsx
@@ -5,5 +5,10 @@ export const LoadButton = () => {
   const handleClick = () => {
     load();
   };
-  return <button onClick={handleClick}>load upd from Github</button>;
+  return (
+    <>
+      <p>Click on this button</p>
+      <button onClick={handleClick}>load upd from Github</button>
+    </>
+  );
 };
